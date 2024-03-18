87eb5d97082ad1b982f81bd95f7a8bf757e7c506
Mohammad Taha <mtaha10045@gmail.com>
2024-03-18 18:34:15 +0500

    update from pc

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import strength from '../../public/register.jpg';
 
 export default function About() {
   return (
-    <div>
-      About
+    <div style={{ "height": "90vh" }} className='position-relative overflow-hidden section-2'>
+      <img src={strength} className='w-100 d-block h-100 object-fit-cover' alt="" />
+      <div className=' position-absolute start-0 text-center w-100  shadow light-light-bg p-4 ' style={{ "top": "18%"}}>
+        <h1 className="text-center text-white fw-bold">
+          About Us
+        </h1>
+        <p className='m-0 p-0 text-white col-md-12 mx-auto p-4 h6' style={{"lineHeight":"32px"}}>
+          Welcome to Fitness Tracker, where we understand the evolving landscape of health and fitness in the digital age. In recent years, there has been a significant surge in health and fitness consciousness worldwide. With the advent of technology and the proliferation of smartphones, individuals are increasingly turning to digital solutions to manage and monitor their fitness journeys. This growing demand has paved the way for comprehensive fitness tracking applications that cater to the diverse needs of fitness enthusiasts.
+          <br />
+          <br />
+          At Fitness Tracker, we recognize the need for an intuitive and comprehensive platform to help users track their fitness activities, including workouts, nutrition, and progress over time. Our application is designed to empower users to take control of their health and achieve their fitness goals with ease.
+          <br />
+          <br />
+          Whether you're a seasoned athlete or just beginning your fitness journey, our goal is to provide you with the tools and support you need to succeed. From personalized workout plans to real-time progress tracking, Fitness Tracker is your partner in achieving a healthier, happier lifestyle.
+          <br />
+          <br />
+          Join us on this journey towards better health and fitness. Start tracking your progress today with Fitness Tracker.
+        </p>
+      </div>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,79 @@
 import React from 'react'
 import Auth from '../services/authServices'
 import section1 from '../../public/home.jpg';
+import calories from '../../public/profile.jpg';
+import macros from '../../public/login.jpg';
+import strength from '../../public/muscular-young-man-lifting-weights-black-background.jpg';
+import cardio from '../../public/black-white-photo-muscular-build-man-using-sports-chalk-hands-while-weightlifting-gym.jpg';
 import { NavLink } from 'react-router-dom';
-export default function Home() {
-    const { isAuthenticated } = Auth();
+import $ from 'jquery';
 
+export default function Home() {
+
+    const { isAuthenticated } = Auth();
     return (
-        <div style={{ "height": "90vh" }} className='position-relative'>
-            <img src={section1} className='w-100 d-block h-100 object-fit-cover' alt="" />
-            <div className=' position-fixed start-0 text-center w-100' style={{ "top": "40%" }}>
-                <h1 className='m-0 p-0 text-white col-md-8 mx-auto fw-bold p-5 rounded-pill'>Train hard, track progress, transform yourself.</h1>
-                <div className="input-group p-0 m-0 my-0 justify-content-center col-md-8 mx-auto">
-                    <NavLink to="/workout" className="btn btn-outline-light">
-                        Start Workout
-                    </NavLink>
-                    <NavLink to="/nutrition" className="btn btn-outline-light">
-                        Track Nutritions
-                    </NavLink>
+        <>
+            <div style={{ "height": "90vh" }} className='position-relative overflow-hidden section-1'>
+                <img src={section1} className='w-100 d-block h-100 object-fit-cover' alt="" />
+                <div className=' position-fixed start-0 text-center w-100 z-1ndex--1 light-light-bg' style={{ "top": "40%" }}>
+                    <h1 className='m-0 p-0 text-white col-md-8 mx-auto fw-bold p-5 rounded-pill'>Train hard, track progress, transform yourself.</h1>
+                    <div className="input-group p-0 m-0 my-0 justify-content-center col-md-8 mx-auto">
+                        <NavLink to="/workout" className="btn btn-outline-light">
+                            Start Workout
+                        </NavLink>
+                        <NavLink to="/nutrition" className="btn btn-outline-light">
+                            Track Nutritions
+                        </NavLink>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div style={{ "height": "90vh" }} className='position-relative overflow-hidden section-2'>
+                <img src={calories} className='w-100 d-block h-100 object-fit-cover' alt="" />
+                <div className=' position-absolute start-0 text-center w-100' style={{ "top": "38%" }}>
+                    <h1 className='m-0 p-0 text-white col-md-12 mx-auto fw-bold p-4  shadow light-light-bg'>Track Your Calories.
+                        <br />
+                        <NavLink to="/nutrition" className="btn btn-outline-light btn-lg my-3">
+                            Let's Track
+                        </NavLink>
+                    </h1>
+                </div>
+            </div>
+
+            <div style={{ "height": "90vh" }} className='position-relative overflow-hidden section-2'>
+                <img src={strength} className='w-100 d-block h-100 object-fit-cover' alt="" />
+                <div className=' position-absolute start-0 text-center w-100' style={{ "top": "38%" }}>
+                    <h1 className='m-0 p-0 text-white col-md-12 mx-auto fw-bold p-4  shadow light-light-bg'>Track Your Strength.
+                        <br />
+                        <NavLink to="/workout" className="btn btn-outline-light btn-lg my-3">
+                            Let's Track
+                        </NavLink>
+                    </h1>
+                </div>
+            </div>
+            <div style={{ "height": "90vh" }} className='position-relative overflow-hidden section-2'>
+                <img src={macros} className='w-100 d-block h-100 object-fit-cover' alt="" />
+                <div className=' position-absolute start-0 text-center w-100' style={{ "top": "38%" }}>
+                    <h1 className='m-0 p-0 text-white col-md-12 mx-auto fw-bold p-4  shadow light-light-bg'>Track Your Macros.
+                        <br />
+                        <NavLink to="/nutrition" className="btn btn-outline-light btn-lg my-3">
+                            Let's Track
+                        </NavLink>
+                    </h1>
+                </div>
+            </div>
+            <div style={{ "height": "90vh" }} className='position-relative overflow-hidden section-2'>
+                <img src={cardio} className='w-100 d-block h-100 object-fit-cover' alt="" />
+                <div className=' position-absolute start-0 text-center w-100' style={{ "top": "38%" }}>
+                    <h1 className='m-0 p-0 text-white col-md-12 mx-auto fw-bold p-4  shadow light-light-bg'>Track Your Cardio.
+                        <br />
+                        <NavLink to="/workout" className="btn btn-outline-light btn-lg my-3">
+                            Let's Track
+                        </NavLink>
+                    </h1>
+                </div>
+            </div>
+
+        </>
+
     )
 }
